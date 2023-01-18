94bf6f2defc3d18fb50bb75cce2cc6d2edd40204
Maciej Rzadzinski <maciej.rzd@gmail.com>
2023-01-18 17:36:58 +0100

    Print data to test methods, write README

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# binary-search-trees-odin
+# Binary Search Trees Odin
+
+This project was an extensive practice of binary search trees and various operations on them.
+
+## Technologies
+* Javascript
+
+## Skills practiced
+* Binary search trees
+
+## Acknowledgments
+* This project was an assignment from The Odin Project course: [Binary Search Trees](https://www.theodinproject.com/lessons/javascript-binary-search-trees)
--- bst.js
+++ bst.js
@@ -1,17 +1,19 @@
+/* eslint-disable consistent-return */
+/* eslint-disable no-unused-expressions */
 /* eslint-disable no-param-reassign */
 
 const Node = (data, left = null, right = null) => ({ data, left, right });
 
 const Tree = (array) => {
-  const prepareArray = () => {
-    const noDuplicates = array.filter(
-      (item, index) => array.indexOf(item) === index
+  const prepareArray = (arr = array) => {
+    const noDuplicates = arr.filter(
+      (item, index) => arr.indexOf(item) === index
     );
     const sorted = noDuplicates.sort((prev, next) => (prev < next ? -1 : 1));
     return sorted;
   };
 
-  const buildTree = (arr, start, end) => {
+  const buildTree = (arr, start = 0, end = arr.length - 1) => {
     if (start > end) return null;
 
     const mid = Math.round((start + end) / 2);
@@ -24,7 +26,7 @@ const Tree = (array) => {
   };
 
   const arr = prepareArray();
-  const root = buildTree(arr, 0, arr.length - 1);
+  let root = buildTree(arr);
 
   const insertNode = (value, rt = root) => {
     if (rt === null) {
@@ -65,15 +67,15 @@ const Tree = (array) => {
     return rt;
   };
 
-  function findValue(value, rt = root) {
+  const findValue = (value, rt = root) => {
     if (rt === null) return rt;
     if (value < rt.data) return findValue(value, rt.left);
     if (value > rt.data) return findValue(value, rt.right);
 
     return rt;
-  }
+  };
 
-  function levelOrder() {
+  const levelOrder = (callback) => {
     const values = [];
     const queue = [root];
 
@@ -81,16 +83,97 @@ const Tree = (array) => {
       const tempNode = queue.shift();
       values.push(tempNode.data);
 
+      if (callback) callback(tempNode);
+
       if (tempNode.left != null) queue.push(tempNode.left);
       if (tempNode.right != null) queue.push(tempNode.right);
     }
 
-    return values;
-  }
+    return callback ? null : values;
+  };
 
-  function levelOrderRec(rt = root) {}
+  const inorder = (callback, rt = root, values = []) => {
+    if (rt === null) return;
 
-  return { root, insertNode, deleteNode, findValue, levelOrder };
+    inorder(callback, rt.left, values);
+    // eslint-disable-next-line no-unused-expressions
+    callback ? callback(rt) : values.push(rt.data);
+    inorder(callback, rt.right, values);
+
+    return callback ? null : values;
+  };
+
+  const preorder = (callback, rt = root, values = []) => {
+    if (rt === null) return;
+
+    callback ? callback(rt) : values.push(rt.data);
+    preorder(callback, rt.left, values);
+    preorder(callback, rt.right, values);
+
+    return callback ? null : values;
+  };
+
+  const postorder = (callback, rt = root, values = []) => {
+    if (rt === null) return;
+
+    postorder(callback, rt.left, values);
+    postorder(callback, rt.right, values);
+    callback ? callback(rt) : values.push(rt.data);
+
+    return callback ? null : values;
+  };
+
+  const height = (rt = root) => {
+    if (rt === null) return -1;
+
+    const leftBranch = height(rt.left) + 1;
+    const rightBranch = height(rt.right) + 1;
+
+    return leftBranch > rightBranch ? leftBranch : rightBranch;
+  };
+
+  const depth = (nodeValue, edges = 0, rt = root) => {
+    if (rt === null) return;
+
+    if (nodeValue < rt.data) return depth(nodeValue, edges + 1, rt.left);
+    if (nodeValue > rt.data) return depth(nodeValue, edges + 1, rt.right);
+
+    return edges;
+  };
+
+  const isBalanced = (rt = root) => {
+    if (rt === null) return;
+
+    const leftBranch = height(rt.left) + 1;
+    const rightBranch = height(rt.right) + 1;
+
+    if (leftBranch - rightBranch < -1 || leftBranch - rightBranch > 1) {
+      return false;
+    }
+
+    return true;
+  };
+
+  const rebalance = () => {
+    const values = prepareArray(inorder());
+    root =  buildTree(values);
+    return root;
+  };
+
+  return {
+    root,
+    insertNode,
+    deleteNode,
+    findValue,
+    levelOrder,
+    inorder,
+    preorder,
+    postorder,
+    height,
+    depth,
+    isBalanced,
+    rebalance,
+  };
 };
 
 // Helper for deleteNode() method
@@ -113,9 +196,32 @@ const prettyPrint = (node, prefix = '', isLeft = true) => {
 const numbers = [
   1, 7, 4, 23, 8, 9, 4, 3, 7, 9, 67, 6345, 324, 24, 500, 848, 29, 500,
 ];
-const bst = Tree(numbers);
-const newNode = Node(1);
+const tree = Tree(numbers);
+const leaf = tree.findValue(23);
 
-// console.log(bst);
-console.log(bst.levelOrder());
-console.log(prettyPrint(bst.root));
+// Check if tree is balanced
+console.log(`Is balanced: ${tree.isBalanced()}`)
+
+// Print preorder, inorder, postorder
+console.log(`Preorder: ${tree.preorder()}`);
+console.log(`Inorder: ${tree.inorder()}`);
+console.log(`Postorder: ${tree.postorder()}`);
+
+// Unbalance tree
+console.log('Unbalance tree')
+leaf.right = Node(666);
+leaf.right.right = Node(999);
+leaf.right.right.left = Node(333);
+
+// Check if tree is balanced
+console.log(`Is balanced: ${tree.isBalanced()}`)
+
+// Rebalance
+console.log('Rebalance')
+tree.root = tree.rebalance();
+
+// Check if tree is balanced
+console.log(`Is balanced: ${tree.isBalanced()}`)
+
+// Print graph
+console.log(prettyPrint(tree.root));
